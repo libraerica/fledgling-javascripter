7f11724b940f076be58fc737587ff433700aa577
libraerica <erica.lenton@gmail.com>
2012-09-14 00:22:07 -0700

    -new picture more style

--- js/erica.js
+++ js/erica.js
@@ -1,12 +1,42 @@
 var change_bg = function(color) {
 	$("body").css("background-color", color);
+	$("#name_box").html(color);
+}
+var swap = function () {
+	var first = $("#first").html();
+	var second = $("#second").html();
+	$("#first").html(second);
+	$("#second").html(first);
+	attach_callback();
 }
 
+var rand_255 = function () {
+	return Math.floor(Math.random() * 256);
+}
 
-$("#boom_btn").click(function () {change_bg("#5080B0")});
+var random_color = function () {
+	var red = rand_255();
+	var green = rand_255();
+	var blue = rand_255();
+	return "rgb(" + red + ", " + green + ", " + blue + ")"
+}
 
-$("#change_color").submit(function () {
-	var color = $('#change_color input[type="text"]').val()
-	change_bg(color);
+var attach_callback = function () {
+	$("#boom_btn").click(function () {
+		change_bg(random_color());
+	
+	});
 
-});
+	$("#change_color").submit(function () {
+		var color = $('#change_color input[type="text"]').val()
+		change_bg(color);
+		swap();
+	});
+}
+
+attach_callback();
+
+var color = random_color();
+change_bg(color);
+
+setInterval()
